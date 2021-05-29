9bdb117b56de202bddd9537a985b0df7cb29ee5d
yale918 <yale918@gmail.com>
2021-05-30 00:12:51 +0800

    1st deploy commit

---
+++ server/models/post.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose')
+
+const {ObjectId} = mongoose.Schema.Types
+const postSchema = new mongoose.Schema({
+    title:{
+        type:String,
+        required:true
+    },
+    body:{
+        type:String,
+        require:true
+    },
+    photo:{
+        type:String,
+        require:true
+    },
+    likes:[{type:ObjectId,ref:"User"}],   //like API
+    comments:[{             //comment API
+        text:String,
+        postedBy:{type:ObjectId,ref:"User"}
+    }],
+    postedBy:{
+        type:ObjectId,
+        ref:"User"
+    }
+})
+
+mongoose.model("Post",postSchema)
